3aae4e2f4027a827754eb7f0c9b49bb3f2043022
GAMELOFT\hoa.tranbinh <hoa.tranbinh@gameloft.com>
2024-11-12 14:21:59 +0700

    lession 20 code commit

--- Source/WarriorRPG/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
+++ Source/WarriorRPG/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -22,9 +22,9 @@ class WARRIORRPG_API UWarriorGameplayAbility : public UGameplayAbility
 	GENERATED_BODY()
 protected:
 	//~ Begin UGameplayAbility Interface.
-	virtual void OnGiveAbility(const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilitySpec &Spec);
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilitySpec &Spec) override;
 
-	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	//~ End UGameplayAbility Interface
 
 	UPROPERTY(EditDefaultsOnly, Category = "WarriorAbility")
---
+++ Source/WarriorRPG/Public/Items/Weapons/WarriorHeroWeapon.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/WarriorWeaponBase.h"
+#include "WarriorHeroWeapon.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class WARRIORRPG_API AWarriorHeroWeapon : public AWarriorWeaponBase
+{
+	GENERATED_BODY()
+};
