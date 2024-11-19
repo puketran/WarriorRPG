c488f5221dc92436fe88713dc694cd3531117851
GAMELOFT\hoa.tranbinh <hoa.tranbinh@gameloft.com>
2024-11-19 17:14:01 +0700

    lession 36 done

--- Source/WarriorRPG/Public/WarriorGameplayTags.h
+++ Source/WarriorRPG/Public/WarriorGameplayTags.h
@@ -14,4 +14,7 @@ namespace WarriorGameplayTags
 
 	/** Player tags */
 	WARRIORRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Weapon_Axe);
+
+	WARRIORRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Equip_Axe);
+	WARRIORRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Unequip_Axe);
 }
